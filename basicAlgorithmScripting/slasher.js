2804900543dc77d7dca314ef6d1273580eb931b6
agerista <amandacrawfordcodes@gmail.com>
2017-04-18 19:47:01 +0000

    clean up code

--- basicAlgorithmScripting/slasher.js
+++ basicAlgorithmScripting/slasher.js
@@ -1,5 +1,7 @@
 function slasher(arr, howMany) {
   // it doesn't always pay to be first
+  // Return the remaining elements of an array after chopping off n elements 
+  // from the head.
   
   var newArr = [];
   newArr = arr.slice(howMany);
@@ -7,4 +9,8 @@ function slasher(arr, howMany) {
   return newArr;
 }
 
-slasher([1, 2, 3], 2);
+slasher([1, 2, 3], 2); // 3
+slasher([1, 2, 3], 0); // [1, 2, 3]
+slasher([1, 2, 3], 9); // []
+slasher(["burgers", "fries", "shake"], 1); // ["fries", "shake"]
+slasher([1, 2, "chicken", 3, "potatoes", "cheese", 4], 5); // ["cheese", 4]
