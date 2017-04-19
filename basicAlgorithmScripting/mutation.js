b908f93793a8937918f628560f1f146c898910da
agerista <amandacrawfordcodes@gmail.com>
2017-04-19 04:25:04 +0000

    check if first element of the array contains all of the letters of the second element in the array

---
+++ basicAlgorithmScripting/mutation.js
@@ -0,0 +1,29 @@
+function mutation(arr) {
+// Return true if the string in the first element of the array contains all of 
+// the letters of the string in the second element of the array.
+  
+  var check = arr[0].toLowerCase();
+  var other = arr[1].toLowerCase();
+  var result = true;
+  
+  for (var i=0; i<other.length; i++) {
+    console.log(other[i]);
+    console.log(check.indexOf(other[i]) !== -1);
+    if (check.indexOf(other[i]) === -1){
+      result = false;
+    }
+  }
+  
+  return result;
+   
+}
+
+mutation(["hello", "hey"]); // false.
+mutation(["hello", "Hello"]); // true.
+mutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"]); // true.
+mutation(["Mary", "Army"]); // true.
+mutation(["Mary", "Aarmy"]); // true.
+mutation(["Alien", "line"]); // true.
+mutation(["floor", "for"]); // true.
+mutation(["hello", "neo"]); // false.
+mutation(["voodoo", "no"]); // false.
